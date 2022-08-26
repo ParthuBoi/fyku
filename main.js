292ed533127e8efc822604aed8dd9ab3bd8ab806
ParthuBoi <100480418+ParthuBoi@users.noreply.github.com>
2022-08-26 19:38:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+song = "";
+function preload()
+{
+    song = loadSound("n.mp3");
+}
+
+function setup() {
+	canvas = createCanvas(500,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,500,500);
+    
+}
+
+function play(){
+    song.play();
+}
+
+
